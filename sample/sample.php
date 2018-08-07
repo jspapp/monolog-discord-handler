a67123f531b24c970af27d40b4dc83774e5f83df
Jason Papp <jspapp@gmail.com>
2018-08-07 03:44:09 +0000

    Update sample to match functionality

--- sample/sample.php
+++ sample/sample.php
@@ -1,15 +1,12 @@
 <?php
 
+use jspapp\DiscordHandler\DiscordHandler;
+
 require '../vendor/autoload.php';
 
-$webhook = 'https://discordapp.com/api/webhooks/[your webhook here]';
+$webhook = 'https://discordapp.com/api/webhooks/xxxxxxxxxxxxxxxxxx/yyyyyyyyyyyyyyyyyyyyyy';
 
-$logger = new Monolog\Logger('log');
-$logger->pushHandler(new jspapp\DiscordHandler\DiscordHandler(
-	[
-		$webhook,
-	],
-	Monolog\Logger::DEBUG
-));
+$logger = new Monolog\Logger('local');
+$logger->pushHandler(new DiscordHandler($webhook));
 
-$logger->info('User created', ['user_id' => 1, 'ip' => '127.0.0.1']);
+$logger->error('User created.', ['user_id' => 1, 'ip' => '127.0.0.1']);
